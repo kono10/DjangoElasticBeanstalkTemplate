bd288ae42d407382e71040cee3240e8843d0cd1f
Jake Konovsky <jkonovsky@DRWHoldings.com>
2022-06-25 09:23:46 -0500

    commit changes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "prod_static")
 STATIC_URL = "/static/"
 
 # Default primary key field type
